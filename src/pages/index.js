6643a421d6cb8dda38f0aad09256bba353d35208
rejosu <russell.e.johnston@gmail.com>
2020-10-26 20:57:24 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people, Test {" "}
+          Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
