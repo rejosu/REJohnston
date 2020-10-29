cf79e87513dd6be1c78dd93ec47ab9c40b9ae053
rejosu <russell.e.johnston@gmail.com>
2020-10-29 10:10:37 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `RE:Johnston Blog`,
+    author: `Russell (Edward) Johnston`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
